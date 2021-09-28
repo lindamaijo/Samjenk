a16df7e75ca38dfce9a0faa5b5fb1d4a4b3a2ee9
username <emailaddress@yahoo.com>
2021-09-28 15:32:51 +0530

    python file add

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("hello world")
+print("how are you")
+
+name = input("what is your Name?:")
+print("hello"+name)
+age = int(input("how old are you"))
+age = age+1
+print("your age is "+str(age) + "years old")
